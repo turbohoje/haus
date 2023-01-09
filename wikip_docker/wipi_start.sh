a00bef15f2d3fc62eec201d41b3c8c76db112ff2
turbohoje <turbohoje@gmail.com>
2023-01-08 19:13:37 -0700

    wipi start command

---
+++ wikip_docker/wipi_start.sh
@@ -0,0 +1,22 @@
+#https://hub.docker.com/r/linuxserver/unifi-controller
+
+docker run -d \
+  --name=unifi-controller \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e JMEM_LIMIT=900M `#optional` \
+  -p 3478:3478/udp \
+  -p 10001:10001/udp \
+  -p 8080:8080 \
+  -p 8443:8443 \
+  -p 1900:1900/udp `#optional` \
+  -p 8843:8843 `#optional` \
+  -p 8880:8880 `#optional` \
+  -p 6789:6789 `#optional` \
+  -p 5514:5514/udp `#optional` \
+  -v /mnt/wipi2022:/config \
+  --restart unless-stopped \
+  lscr.io/linuxserver/unifi-controller:latest
+#ghcr.io/linuxserver/unifi-controller:7.1.66-ls151
+
+#  -v /home/turbohoje/run:/etc/services.d/unifi/run \
