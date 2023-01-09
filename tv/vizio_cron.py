e2f3bd225c51797ee30962ddf29f41d2e6b793a4
turbohoje <turbohoje@gmail.com>
2023-01-08 18:26:33 -0700

    added dark start thread

--- tv/vizio_cron.py
+++ tv/vizio_cron.py
@@ -1,7 +1,7 @@
 #!/bin/env python3
 #pip3 install pyvizio
 
-import pyvizio, os, time, sys, getopt
+import pyvizio, os, time, sys, getopt, threading
 key = ""
 secdelay = 3600
 try:
@@ -20,7 +20,8 @@ tvs = {
     "master":{
         "ip":"10.22.14.134",
         "auth":"Zdmx9r0pcx",
-        "input":"HDMI-3"
+        "input":"HDMI-3",
+        "backlight": (60*30)
     },
     "kitchen":{
         "ip":"10.22.14.133",
@@ -30,14 +31,37 @@ tvs = {
 }
 
 a = pyvizio.Vizio("pyvizio", tvs[key]['ip'], key, tvs[key]['auth'])
+# a.set_setting("picture", "backlight", 50)
+print(a.get_current_input())
+exit(1)
+def brightness_inc(val):
+    #increase the brightness in 5 steps over val seconds
+    #sleep to let the input select rest
+    time.sleep(3)
+    for inc in range(1, 102, 20):
+        if inc > 100:
+            inc = 100
+        print("testing setting bright to " + str(inc))
+        a.set_setting("picture", "backlight", int(inc))
+        time.sleep(int(int(val)/5))
+    print("brightness done")
 
-if a.get_power_state() == False:
+if a.get_power_state() == True:
     print(key + " tv is off, powering on and setting input")
     a.pow_on()
     time.sleep(3)
+    if tvs[key].get('backlight') is not None:
+        print("backlight!!")
+        x = threading.Thread(target=brightness_inc(tvs[key]['backlight']), args=(1,))
+        x.start()
     a.set_input(tvs[key]['input'])
     time.sleep(secdelay)
-    print("Shutting down")
-    a.pow_off()
+
+    #only shutdown if the input hasnt been changed.
+    if a.get_current_input() == tvs[key]['input']:
+        print("Shutting down")
+        a.pow_off()
+    else:
+        print("input changed, assume someone is watching the tv")
 else:
     print(key + " tv is on, skipping")
