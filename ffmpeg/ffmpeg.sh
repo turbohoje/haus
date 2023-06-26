993bfaa844ec0a0e12496d07d8c66f51de598199
Justin Meyer <jmeyer@crusoeenergy.com>
2023-06-26 08:52:42 -0600

    ffplay example, input correction

--- ffmpeg/ffmpeg.sh
+++ ffmpeg/ffmpeg.sh
@@ -11,34 +11,31 @@ ip=10.22.14.9
 
 #-loglevel 99 -report -v 9 \
 #args="[0:v]scale=960:540[one];[1:v]scale=960:540[two];[2:v]scale=960:540[three];[3:v]scale=960:540[four];[one][two]hstack=inputs=2[top];[three][four]hstack=inputs=2[bottom];[top][bottom]vstack=inputs=2"
-
-args="[0:v][1:v]hstack=inputs=2[top];[2:v][3:v]hstack=inputs=2[bottom];[top][bottom]vstack=inputs=2"
-
-
-#ffmpeg   \
-#-i "rtmp://$ip/bcs/channel0_sub.bcs?channel=0&stream=1&user=$un&password=$pass" \
-#-i "rtmp://$ip/bcs/channel1_sub.bcs?channel=0&stream=1&user=$un&password=$pass" \
-#-i "rtmp://$ip/bcs/channel2_sub.bcs?channel=0&stream=1&user=$un&password=$pass" \
-#-i "rtmp://$ip/bcs/channel3_sub.bcs?channel=0&stream=1&user=$un&password=$pass" \
-#-filter_complex $args \
-#-pix_fmt bgra -f fbdev /dev/fb0
-
-/h264Preview_01_sub
-
+#args="[0:v][1:v]hstack=inputs=2[top];[2:v][3:v]hstack=inputs=2[bottom];[top][bottom]vstack=inputs=2"
 #testargs="[0:v]scale=1920:1080[bg];[bg][1:v]overlay"
-
 testargs="[0:v]scale=1920:1080[bg];[1:v][2:v][3:v]vstack=inputs=3[stk];[bg][stk]overlay"
 
-#ffmpeg -err_detect aggressive -fflags discardcorrupt  \
-#-i "rtsp://$un:$pass@$ip:554/h264Preview_01_sub" \
-#-i "rtsp://$un:$pass@$ip:554/h264Preview_02_sub" \
-#-i "rtsp://$un:$pass@$ip:554/h264Preview_04_sub" \
-#-i "rtsp://$un:$pass@$ip:554/h264Preview_03_sub" \
-ffmpeg -err_detect aggressive -fflags discardcorrupt  \
--i "rtsp://$un:$pass@$ip:554/h264Preview_04_sub" \
--i "rtsp://$un:$pass@$ip:554/h264Preview_01_sub" \
--i "rtsp://$un:$pass@$ip:554/h264Preview_05_sub" \
--i "rtsp://$un:$pass@$ip:554/h264Preview_03_sub" \
--filter_complex $testargs \
--pix_fmt bgra -f fbdev /dev/fb0
+if [ "$#" -ne 1 ]; then
+  # fb pi output
+  echo "output to framebuffer"
+  ffmpeg -err_detect aggressive -fflags discardcorrupt  \
+  -i "rtsp://$un:$pass@$ip:554/h264Preview_04_sub" \
+  -i "rtsp://$un:$pass@$ip:554/h264Preview_01_sub" \
+  -i "rtsp://$un:$pass@$ip:554/h264Preview_05_sub" \
+  -i "rtsp://$un:$pass@$ip:554/h264Preview_02_sub" \
+  -filter_complex $testargs \
+  -pix_fmt bgra -f fbdev /dev/fb0
 
+else
+
+  echo "output to ffplay"
+  # osx ouput
+  ffmpeg -err_detect aggressive -fflags discardcorrupt  \
+  -i "rtsp://$un:$pass@$ip:554/h264Preview_04_sub" \
+  -i "rtsp://$un:$pass@$ip:554/h264Preview_01_sub" \
+  -i "rtsp://$un:$pass@$ip:554/h264Preview_05_sub" \
+  -i "rtsp://$un:$pass@$ip:554/h264Preview_02_sub" \
+  -filter_complex $testargs \
+  -f matroska - | ffplay -i -
+
+fi
