90647ea6075c9cf8ab9080d653083170cebbba10
Justin Meyer <jmeyer@appomni.com>
2023-12-20 17:54:17 -0700

    adjust and crop for new cam

--- ffmpeg/ffmpeg.sh
+++ ffmpeg/ffmpeg.sh
@@ -13,7 +13,8 @@ ip=10.22.14.9
 #args="[0:v]scale=960:540[one];[1:v]scale=960:540[two];[2:v]scale=960:540[three];[3:v]scale=960:540[four];[one][two]hstack=inputs=2[top];[three][four]hstack=inputs=2[bottom];[top][bottom]vstack=inputs=2"
 #args="[0:v][1:v]hstack=inputs=2[top];[2:v][3:v]hstack=inputs=2[bottom];[top][bottom]vstack=inputs=2"
 #testargs="[0:v]scale=1920:1080[bg];[bg][1:v]overlay"
-testargs="[0:v]scale=1920:1080[bg];[1:v][2:v][3:v]vstack=inputs=3[stk];[bg][stk]overlay"
+#testargs="[0:v]scale=1920:1080[bg];[1:v][2:v][3:v]vstack=inputs=3[stk];[bg][stk]overlay"
+testargs="[0:v]crop=900:576:200:0,scale=1920:1080[bg];[1:v][2:v][3:v]vstack=inputs=3[stk];[bg][stk]overlay"
 
 if [ "$#" -ne 1 ]; then
   # fb pi output
