a17320bb97fd30137299d2cf4766f5f7005bb30e
Justin <justin@rocketscience.cc>
2023-12-30 14:58:02 -0700

    working OAT

--- ffmpeg/img.sh
+++ ffmpeg/img.sh
@@ -9,7 +9,7 @@ nvr=10.22.14.9
 
 mkdir -p ./imgproc
 
-while [ 1 ]; do
+#while [ 1 ]; do
     # nvr images, (slower)
     #time curl -k -s "https://${ip}/cgi-bin/api.cgi?cmd=Snap&channel=0&user=${un}&password=${pass}" -o imgproc/0.jpg
     #time curl -k -s "https://${ip}/cgi-bin/api.cgi?cmd=Snap&channel=1&user=${un}&password=${pass}" -o imgproc/1.jpg
@@ -18,17 +18,21 @@ while [ 1 ]; do
     #time curl -k -s "https://${ip}/cgi-bin/api.cgi?cmd=Snap&channel=4&user=${un}&password=${pass}" -o imgproc/4.jpg
     #direct from camera
 
-    time curl -k -s "https://10.22.14.47/cgi-bin/api.cgi?cmd=Snap&channel=0&user=${un}&password=${pass}" -o imgproc/0.jpg &
-    time curl -k -s "https://${nvr}/cgi-bin/api.cgi?cmd=Snap&channel=1&user=${un}&password=${pass}" -o imgproc/1.jpg &
-    time curl -k -s "https://${nvr}/cgi-bin/api.cgi?cmd=Snap&channel=2&user=${un}&password=${pass}" -o imgproc/2.jpg & 
-    time curl -k -s "https://10.22.14.49/cgi-bin/api.cgi?cmd=Snap&channel=0&user=${un}&password=${pass}" -o imgproc/3.jpg & 
-    time curl -k -s "https://10.22.14.48/cgi-bin/api.cgi?cmd=Snap&channel=0&user=${un}&password=${pass}" -o imgproc/4.jpg &
+    curl -k -s "https://10.22.14.47/cgi-bin/api.cgi?cmd=Snap&channel=0&user=${un}&password=${pass}" -o imgproc/0.jpg &
+    curl -k -s "https://${nvr}/cgi-bin/api.cgi?cmd=Snap&channel=1&user=${un}&password=${pass}" -o imgproc/1.jpg &
+    curl -k -s "https://${nvr}/cgi-bin/api.cgi?cmd=Snap&channel=2&user=${un}&password=${pass}" -o imgproc/2.jpg & 
+    curl -k -s "https://10.22.14.49/cgi-bin/api.cgi?cmd=Snap&channel=0&user=${un}&password=${pass}" -o imgproc/3.jpg & 
+    curl -k -s "https://10.22.14.48/cgi-bin/api.cgi?cmd=Snap&channel=0&user=${un}&password=${pass}" -o imgproc/4.jpg &
+
+    oat_c=$(curl -s "http://10.22.14.4:3480/data_request?id=variableget&DeviceNum=112&serviceId=urn:upnp-org:serviceId:TemperatureSensor1&Variable=CurrentTemperature")
+    oat_f=$(echo "($oat_c * 9/5) + 32" | bc)
+
 
     wait
     small_dims="scale=640:360"
     #,scale=1920:1080
-    testargs="[0:v]crop=0:0:1920:1080[bg];[1:v]$small_dims[1];[2:v]$small_dims[2];[3:v]$small_dims[3];[1][2][3]vstack=inputs=3[stk];[bg][stk]overlay"
-    testargs="[0:v]crop=1920:1080:1726:365[bg];[1:v]$small_dims[1];[2:v]$small_dims[2];[3:v]$small_dims[3];[1][2][3]vstack=inputs=3[stk];[bg][stk]overlay"
+    testargs="[0:v]crop=2520:1380:1326:365,scale=1920:1080[bg];[1:v]$small_dims[1];[2:v]$small_dims[2];[3:v]$small_dims[3];[1][2][3]vstack=inputs=3[stk];[bg][stk]overlay"
+    testargs="$testargs,drawtext=fontfile=/home/turbohoje/haus/ffmpeg/Arial.ttf:text='$oat_c°C/$oat_f°F':fontcolor=white:fontsize=64:box=1:boxcolor=black@0.4:boxborderw=10:x=1500:y=0"
 
     ffmpeg -err_detect aggressive -fflags discardcorrupt  \
     -i "imgproc/3.jpg" \
@@ -41,4 +45,4 @@ while [ 1 ]; do
     
     echo "done"
     
-done
+# done
